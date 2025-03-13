0d2884271f504d2c14594c8c024d560e1fd7f371
Serj Agopian <serjagopian@gmail.com>
2025-03-13 15:11:14 +0100

    Use @react-native-community/cli

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ root=$(pwd)
 rm -rf ./react-native
 mkdir -p ./react-native
 cd ./react-native
-npx react-native init ReactNativeKit --version 0.72.3
+npx @react-native-community/cli init ReactNativeKit
 cd ./ReactNativeKit
 npm install \
     @react-navigation/native@6.1.7 \
